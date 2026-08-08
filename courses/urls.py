47fdbf7ee82342827e9b04861668edfd43d7194f
Shohzod Rahamov <rahamovshohzod@gmail.com>
2026-08-08 23:18:59 +0500

    Prepare project for deployment: env-based settings, whitenoise, fix requirements.txt

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('kurslar/', views.kurslar_royxati, name='kurslar_royxati'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Course, CourseCategory
+
 
 def home(request):
     return render(request, 'courses/home.html')
+
+
+def kurslar_royxati(request):
+    kurslar = Course.objects.all()
+    return render(request, 'courses/courses.html', {'kurslar': kurslar})
